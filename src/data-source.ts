4bdc11d381c3c7a7d2536f7980fbea90629764f7
Seven Du <1835547223@qq.com>
2023-09-22 10:51:22 +0800

    修改一下打包后的数据库url

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,9 @@
 import "reflect-metadata"
 import {DataSource, DataSourceOptions} from "typeorm"
 import * as process from "process";
-// const DEVELOPMENT_ID="192.168.1.179"
-const DEVELOPMENT_ID="localhost"
-const PRODUCTION_ID="localhost"
+const DEVELOPMENT_ID="192.168.1.179"
+// const PRODUCTION_ID="localhost"
+const PRODUCTION_ID="172.17.0.1"
 export const dataSourceConfig: DataSourceOptions={
     type: "postgres",
     host: process.env.NODE_ENV==='production'?PRODUCTION_ID:DEVELOPMENT_ID,
